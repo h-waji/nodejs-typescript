2bb5498a48999f34c1111bc3bcfdf50f59bf722f
wajima <goo.hiro.w@gmail.com>
2023-04-29 16:58:55 +0900

    router

--- dist/app.js
+++ dist/app.js
@@ -5,5 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express'); // => CommonJSの構文
 const express_1 = __importDefault(require("express")); // => ESモジュールの構文
+const todos_1 = __importDefault(require("./routes/todos"));
 const app = (0, express_1.default)();
+app.use('/todos', todos_1.default);
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
 app.listen(3000);
---
+++ src/routes/todos.ts
@@ -0,0 +1,15 @@
+// const express = require('express');
+// const Router = express.Router;
+import { Router } from "express";
+
+const router = Router();
+
+router.post('/');
+
+router.get('/');
+
+router.patch('/:id');
+
+router.delete('/:id');
+
+export default router;
